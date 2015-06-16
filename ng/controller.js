feb3434e968bdecce5320aa8a0de293d780675b8
Michael Burson <michael.burson@razoroccam.com>
2015-06-16 10:04:51 +0100

    Show a change when a pomodoro section is complete

--- ng/controller.js
+++ ng/controller.js
@@ -4,12 +4,15 @@ app.controller( 'TimerCtrl', function( $scope, $interval ) {
     $scope.time = 25*60;
     $scope.minutesLeft = 25;
     $scope.secondsLeft = 0;
+    $scope.pomsCompleted = 0;
     
     var stop;
     
     $scope.startTimer = function() {
         // Dont start a new timer if one is already running
         if ( angular.isDefined(stop) ) return;
+            $('body').css(
+                'background-image', 'url("assets/img/broken-plane.jpg")');
         
         stop = $interval( function() {
             if ( $scope.time > 0 ) {
@@ -20,10 +23,7 @@ app.controller( 'TimerCtrl', function( $scope, $interval ) {
                 if ($scope.secondsLeft === -1) $scope.secondsLeft = 59;
                 
                 
-                $scope.secondsAngle = $scope.time*6;
-                $scope.minutesAngle = $scope.minutesLeft*6;
-                $('.second-hand').css('transform', 'rotate(' + $scope.secondsAngle + 'deg)');
-                $('.minute-hand').css('transform', 'rotate(' + $scope.minutesAngle + 'deg)');
+                $scope.animateHands();
                 
             } else {
                 $scope.resetTimer();
@@ -32,23 +32,44 @@ app.controller( 'TimerCtrl', function( $scope, $interval ) {
     };
     
     $scope.stopTimer = function() {
-        if ( angular.isDefined(stop) ) {
+        if ( angular.isDefined( stop ) ) {
             $interval.cancel( stop );
             stop = undefined;
         }
     };
     
+    $scope.finishTimer = function() {
+        if ( angular.isDefined( stop ) ) {
+            $scope.resetTimer();
+            $scope.pomsCompleted = $scope.pomsCompleted + 1;
+        }
+    };
+    
     $scope.resetTimer = function() {
-        if (angular.isDefined( stop ) ) {
+        if ( angular.isDefined( stop ) ) {
             $scope.stopTimer();
+            $scope.time = 25*60;
             $scope.minutesLeft = 25;
             $scope.secondsLeft = 0;
+            $('body').css(
+                'background-image', 'url("assets/img/evening-water.jpg")');
+            $scope.animateHands();
         }
     };
     
     $scope.$on( '$destroy', function() {
         $scope.stopTimer();
     } );
+    
+    $scope.animateHands = function() {
+        $scope.secondsAngle = $scope.time*6;
+        $scope.minutesAngle = $scope.minutesLeft*6;
+        $('.second-hand').css(
+            'transform', 'rotate(' + $scope.secondsAngle + 'deg)');
+        $('.minute-hand').css(
+            'transform', 'rotate(' + $scope.minutesAngle + 'deg)');
+    };
+    $scope.animateHands();
     
 });
 
